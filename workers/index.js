4a46d1fa97b881b8e1e331108a783f4a7543f87f
HozefaMJ <hozefa24imp@gmail.com>
2020-09-24 11:24:38 +0530

    first commit

--- workers/index.js
+++ workers/index.js
@@ -3,5 +3,5 @@ var CronJob = require('cron').CronJob;
 const fetchGithub = require('./tasks/fetch-github');
 
 
-new CronJob("1 * * * * *", fetchGithub,null,true, "America/Los_Angeles");
+new CronJob("0 12 * * 1", fetchGithub,null,true, "America/Los_Angeles");
 
